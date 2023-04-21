4975a8c5111feed8a8cac0f56f944e3d1e5ad01a
marco.marasciuolo <marco.marasciuolo@studenti.unimi.it>
2023-04-21 02:28:18 +0200

    Primi step con commenti di riferimento

--- firstApp/src/main/java/org/example/App.java
+++ firstApp/src/main/java/org/example/App.java
@@ -1,8 +1,7 @@
 package org.example;
 
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.FileSystemResource;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 /**
  * Hello world!
@@ -13,9 +12,16 @@ public class App
     public static void main( String[] args )
     {
 
-        BeanFactory beanFactory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
-        Dummy dummy = (Dummy) beanFactory.getBean("Dummy");
+        //BeanFactory beanFactory = new XmlBeanFactory(new FileSystemResource("spring.xml"));
+        ApplicationContext factory = new ClassPathXmlApplicationContext("spring.xml");
+        /* una volta che specifico l'id del bean nell xml anche commentando le due righe sotto stanti l'oggetto verrà
+        * istanziato nello spring container e sarà singleton. Quindi anche se dovessi creare un dummy2
+        * dummy e dummy2 faranno riferimento alla stessa istanza di Dummy (singleton) */
+        Dummy dummy = (Dummy) factory.getBean("Dummy");
         dummy.sayhi();
+
+        // come faccio a ricevere una nuova istanza dello stesso oggetto? -> file xml modifica dello scope
+        // di default è singleton -> prototype in questo modo però l'oggetto verrà creato solo quando richiesto
 
     }
 }
